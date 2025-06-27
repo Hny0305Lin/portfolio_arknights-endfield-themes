99f678e3379ac49630faff7e9403432dbafdf288
Burial0268 <Burial0268@foxmail.com>
2025-06-28 02:26:21 +0800

    删除了不再使用的配置文件，更新了 package.json 以添加新依赖，修改了 tsconfig.json 以支持路径别名和 JSX，更新了布局和首页以反映新的组件结构和样式。

---
+++ astro.config.ts
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+    server: {
+      allowedHosts: ['bzsr-devenvlaptop02.taila30f9f.ts.net']
+    }
+  },
+
+  integrations: [react()]
+});
